df147bb6200fe580a82b323b1103b3c550cddc2f
Vitto2623 <vtr.1@icloud.com>
2022-02-23 23:01:58 +0100

    corretto bug bottone annulla

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 // Aggiungo una funzionalità al bottone
 const bottoneGenera = document.getElementById('genera');
 bottoneGenera.addEventListener('click', function(){
+    // età passeggero
+    const etàUtente = document.getElementById('età-utente').value;
+    document.getElementById('età-utente').innerHTML;
     // Lunghezza del tragitto
     const lunghezzaTratta = document.getElementById('km-lenght').value;
     document.getElementById('km-lenght').innerHTML;
     const prezzoParziale = lunghezzaTratta * 0.27;
     let prezzoTotale;
-    // età passeggero
-    const etàUtente = document.getElementById('età-utente').value;
-    document.getElementById('età-utente').innerHTML;
     // calcolo sconto
     if (etàUtente == "Minorenne"){
         prezzoTotale = prezzoParziale - prezzoParziale * 0.17;
@@ -23,6 +23,30 @@ bottoneGenera.addEventListener('click', function(){
 }
 );
 // Bottone che annulla i dati inseriti
-function formReset(){
-	document.getElementById("frm1").reset();
-}
+const bottoneElimina = document.getElementById('reset');
+bottoneElimina.addEventListener('click', function(){
+        // Lunghezza del tragitto
+        const lunghezzaTratta = document.getElementById('km-lenght').value;
+        document.getElementById('km-lenght').innerHTML;
+        const prezzoParziale = lunghezzaTratta * 0.27;
+        let prezzoTotale;
+        // età passeggero
+        const etàUtente = document.getElementById('età-utente').value;
+        document.getElementById('età-utente').innerHTML;
+        // calcolo sconto
+        if (etàUtente == "Minorenne"){
+            prezzoTotale = prezzoParziale - prezzoParziale * 0.17;
+        } else if (etàUtente == "Over-65"){
+                prezzoTotale = prezzoParziale - prezzoParziale * 0.33;
+        } else {
+                prezzoTotale = prezzoParziale;
+        }
+        // Annullo valori
+        document.getElementById('output-prezzo-finale').innerHTML = `${prezzoTotale.toFixed(2)} &euro;`;
+        document.getElementById('add-name').innerHTML = document.getElementById('name').value;
+        document.getElementById('km-lenght').value = "";
+        document.getElementById('name').value = "";
+        document.getElementById('add-name').innerHTML = "";
+        document.getElementById('età-utente').value = "";
+        document.getElementById('output-prezzo-finale').innerHTML = "";
+});
